67a95f4194a38a76cde3ed5ff091036fa59d9f0b
dhanunjayavarma <dhanunjayavarmak@gmail.com>
2019-05-11 13:34:26 +0530

    Implemented SingleTOn Design Pattern to ParkingLot class so that we can have only one parking lot. And also adding AutomaticTicketSystem class which is responsible for issuing tickets and managing them

--- src/parking_lot/ParkingLot.java
+++ src/parking_lot/ParkingLot.java
@@ -3,9 +3,18 @@ package parking_lot;
 import java.util.Map;
 
 public class ParkingLot {
+	private static ParkingLot parkingLot;
 	Map<String, Car> parkingSpots;
 	AutomatedTicketSystem ticketSystem;
-	private ParkingLot() {
-		
+	private int numberOfParkingSpots;
+	
+	private ParkingLot(int numberOfParkingSpots) {
+		this.numberOfParkingSpots=numberOfParkingSpots;
+	}
+	public static ParkingLot getInstance(int numberOfParkingSpots) {
+		if(parkingLot==null) {
+			parkingLot=new ParkingLot(numberOfParkingSpots);
+		}
+		return parkingLot;
 	}
 }
