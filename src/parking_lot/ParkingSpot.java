c9f022ea53cb89273a4947ad7fd679da02de4081
dhanunjayavarma <dhanunjayavarmak@gmail.com>
2019-05-11 13:20:45 +0530

    adding private constructor to implement singleton Design pattern

--- src/parking_lot/ParkingSpot.java
+++ src/parking_lot/ParkingSpot.java
@@ -1,5 +1,11 @@
 package parking_lot;
 
 public class ParkingSpot {
-	
+	private String number;
+	private boolean free;
+	private Car car;
+	ParkingSpot(String number,Car car){
+		this.number=number;
+		this.car=car;
+	}
 }
